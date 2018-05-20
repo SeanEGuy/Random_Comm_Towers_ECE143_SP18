249567bd820a7315a4930337e879d1493612f2c0
SeanEGuy <sguy@ucsd.edu>
2018-05-19 23:12:20 -0700

    Created make_random_tower() to get random rectangles in the plot. Tower coverages are currently defined by extreme x and y values.

--- commtower.py
+++ commtower.py
@@ -42,13 +42,36 @@ def plot_land(L, W):
     return emptyPlot
 
 
-def place_tower(plotArray)
+def make_random_tower(plotArray)
     '''
-   Places a random rectangle representing one tower's coverage in a
-   given plot of land.
+   Generates a random rectangle representing one tower's coverage in a
+   given plot of land. Ignores rules of overlapping and trimming.
    
    :@param plotArray: numpy array, dimensions represnting a plot of land
    :@return: tuple, coordinates of a rectangle in the plot
    '''
+    import numpy as np
+    import 
     
-    assert
+    assert isinstance(plotArray, np.ndarray) \ #check if a numpy array
+             and bool(len(plotArray.shape) == 2), \ #check two dimensions
+             "Input must be a two-dimensional numpy array."
+    
+    L, W = plotArray.shape #save row number as L and cols as W
+    
+    rWidth = list( np.random.randint(0, W, 2) )
+    rLength = list( np.random.randint(0, L, 2) )
+    #picks two values per dimension to determine length an width
+    rWidth.sort()
+    rLength.sort()
+    #sort coordinates in an increasing order
+    
+    rDimensions = tuple( rLength + rWidth )
+    #Combine coords in pattern of (x1, x2, y1, y2)
+    assert bool(len(rDimensions) == 4) #Checking shape of output
+    
+    return rDimensions
+
+    
+    
+    
